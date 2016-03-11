29385bcb48caff8930b262a3adb1fcb12d798408
Markus Ankenbrand <markus@ankenbrand.me>
2016-03-11 14:02:59 +0100

    Update traitbank trait importer to new schema

--- bin/import_traitbank_traits.py
+++ bin/import_traitbank_traits.py
@@ -3,64 +3,21 @@
 import sys
 import json
 import psycopg2
+from dns.rdatatype import NULL
 
 try:
     conn = psycopg2.connect(dbname='test', user='test', host='127.0.0.1', password='test', port=54322)
 except:
     print "I am unable to connect to the database"
 
-# Setup cv eol_traitbank
-with conn:
-    with conn.cursor() as cur:
-        cur = conn.cursor()
-        cur.execute("SELECT cv_id FROM cv WHERE name='EOL TraitBank'")
-        rows = cur.fetchall()
-        if (len(rows) < 1):
-            print "No EOL TraitBank cv - inserting entry"
-            cur.execute("INSERT INTO cv (name) VALUES ('EOL TraitBank') RETURNING cv_id")
-            rows = cur.fetchall()
-        eol_traitbank_cvid = rows[0][0]
-
-# Setup db eol_traitbank
-with conn:
-    with conn.cursor() as cur:
-        cur = conn.cursor()
-        cur.execute("SELECT db_id FROM db WHERE name='EOL TraitBank'")
-        rows = cur.fetchall()
-        if (len(rows) < 1):
-            print "No EOL TraitBank db - inserting entry"
-            cur.execute("INSERT INTO db (name) VALUES ('EOL TraitBank') RETURNING db_id")
-            rows = cur.fetchall()
-        eol_traitbank_dbid = rows[0][0]
-
-def get_or_insert_dbxref(cvterm, dbid):
-    #cvterm name is varchar(255)
-    cvterm = cvterm[0:255]
+def get_or_insert_cvterm(name, definition):
     with conn:
         with conn.cursor() as cur:
-            cur.execute("SELECT dbxref_id FROM dbxref WHERE accession=%s AND db_id=%s", (cvterm,dbid))
+            cur.execute("SELECT trait_cvterm_id FROM trait_cvterm WHERE definition=%s", (definition,))
             rows = cur.fetchall()
             if (len(rows) < 1):
-                print "dbxref '"+cvterm+"' missing - inserting entry"
-                # I have to reset the cvterm_id serial generator - don't know why this is necessary
-                #cur.execute("""SELECT setval('dbxref_dbxref_id_seq', (SELECT MAX(dbxref_id) FROM dbxref)+1)""")
-                cur.execute("""INSERT INTO dbxref (db_id, accession) VALUES ("""+str(dbid)+""",'"""+cvterm+"""') RETURNING dbxref_id""")
-                rows = cur.fetchall()
-    return rows[0][0]
-
-def get_or_insert_cvterm(cvterm, cvid, dbid):
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute("""SELECT cvterm_id FROM cvterm WHERE name='"""+cvterm+"""' AND cv_id="""+str(cvid))
-            rows = cur.fetchall()
-            if (len(rows) < 1):
-                # First insert dbxref
-                dbxref_id = get_or_insert_dbxref(cvterm, dbid)
-                # Now insert cvterm
-                print "cvterm '"+cvterm+"' missing - inserting entry"
-                # I have to reset the cvterm_id serial generator - don't know why this is necessary
-                cur.execute("""SELECT setval('cvterm_cvterm_id_seq', (SELECT MAX(cvterm_id) FROM cvterm)+1)""")
-                cur.execute("""INSERT INTO cvterm (cv_id, dbxref_id, name) VALUES ("""+str(cvid)+","+str(dbxref_id)+""",'"""+cvterm+"""') RETURNING cvterm_id""")
+                #print "cvterm '"+cvterm+"' missing - inserting entry"
+                cur.execute("INSERT INTO trait_cvterm (name, definition) VALUES (%s, %s) RETURNING trait_cvterm_id", (name, definition))
                 rows = cur.fetchall()
     return rows[0][0]
 
@@ -77,7 +34,7 @@ top_level_cvterms = {"dwc:measurementType": 0,
                      }
 
 for cvterm in top_level_cvterms.keys():
-    top_level_cvterms[cvterm] = get_or_insert_cvterm(cvterm, eol_traitbank_cvid, eol_traitbank_dbid)
+    top_level_cvterms[cvterm] = get_or_insert_cvterm(cvterm, cvterm)
 
 # Get EOL db id for mapping of eol id to organism_id
 with conn:
@@ -105,20 +62,23 @@ for element in data["@graph"]:
             continue
         organism_id = rows[0][0]
 
-        type_cvterm_id = get_or_insert_cvterm(element["dwc:measurementType"]["@id"], eol_traitbank_cvid, eol_traitbank_dbid)
+        type_cvterm_id = get_or_insert_cvterm(element["dwc:measurementType"]["rdfs:label"]["en"], element["dwc:measurementType"]["@id"])
         try:
             value = float(element["dwc:measurementValue"])
-            cur.execute("""INSERT INTO trait_entry (organism_id, type_cvterm_id, value) VALUES ("""+str(organism_id)+","+str(type_cvterm_id)+""","""+str(value)+""") RETURNING trait_entry_id""")
+            cur.execute("INSERT INTO trait_entry (organism_id, type_cvterm_id, value, tb_id, data_point_uri_id) VALUES (%s, %s, %s, %s, %s) RETURNING trait_entry_id",
+                        (organism_id, type_cvterm_id, value, element["@id"], element["data_point_uri_id"]))
             rows = cur.fetchall()
         except (TypeError):
             # The value is an object
-            value_cvterm_id = get_or_insert_cvterm(element["dwc:measurementValue"]["@id"], eol_traitbank_cvid, eol_traitbank_dbid)
-            cur.execute("""INSERT INTO trait_entry (organism_id, type_cvterm_id, value_cvterm_id) VALUES ("""+str(organism_id)+","+str(type_cvterm_id)+""","""+str(value_cvterm_id)+""") RETURNING trait_entry_id""")
+            value_cvterm_id = get_or_insert_cvterm(element["dwc:measurementValue"]["rdfs:label"]["en"], element["dwc:measurementValue"]["@id"])
+            cur.execute("INSERT INTO trait_entry (organism_id, type_cvterm_id, value_cvterm_id, tb_id, data_point_uri_id) VALUES (%s, %s, %s, %s, %s) RETURNING trait_entry_id",
+                        (organism_id, type_cvterm_id, value_cvterm_id, element["@id"], element["data_point_uri_id"]))
             rows = cur.fetchall()
         except (ValueError):
             # The value is a string
-            value_cvterm_id = get_or_insert_cvterm(element["dwc:measurementValue"], eol_traitbank_cvid, eol_traitbank_dbid)
-            cur.execute("""INSERT INTO trait_entry (organism_id, type_cvterm_id, value_cvterm_id) VALUES ("""+str(organism_id)+","+str(type_cvterm_id)+""","""+str(value_cvterm_id)+""") RETURNING trait_entry_id""")
+            value_cvterm_id = get_or_insert_cvterm(None, element["dwc:measurementValue"])
+            cur.execute("INSERT INTO trait_entry (organism_id, type_cvterm_id, value_cvterm_id, tb_id, data_point_uri_id) VALUES (%s, %s, %s, %s, %s) RETURNING trait_entry_id",
+                        (organism_id, type_cvterm_id, value_cvterm_id, element["@id"], element["data_point_uri_id"]))
             rows = cur.fetchall()
         trait_entry_id = rows[0][0]
         for cvterm in top_level_cvterms.keys():
@@ -126,8 +86,9 @@ for element in data["@graph"]:
                 continue
             if(cvterm in element):
                 if(type(element[cvterm]) is dict):
-                    secondary_cvterm_id = get_or_insert_cvterm(element[cvterm]["@id"], eol_traitbank_cvid, eol_traitbank_dbid)
+                    secondary_cvterm_id = get_or_insert_cvterm(element[cvterm]["rdfs:label"]["en"], element[cvterm]["@id"])
                 else:
-                    secondary_cvterm_id = get_or_insert_cvterm(element[cvterm], eol_traitbank_cvid, eol_traitbank_dbid)
-                cur.execute("""INSERT INTO trait_metadata (trait_entry_id, subject_cvterm_id, value_cvterm_id) VALUES ("""+str(trait_entry_id)+","+str(top_level_cvterms[cvterm])+""","""+str(secondary_cvterm_id)+""") RETURNING trait_metadata_id""")
+                    secondary_cvterm_id = get_or_insert_cvterm("", element[cvterm])
+                cur.execute("""INSERT INTO trait_metadata (trait_entry_id, subject_cvterm_id, value_cvterm_id) VALUES (%s, %s, %s) RETURNING trait_metadata_id""",
+                            (trait_entry_id, top_level_cvterms[cvterm], secondary_cvterm_id))
 conn.commit()
