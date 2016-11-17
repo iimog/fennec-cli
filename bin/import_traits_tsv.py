706fdde91b177c97eb123c4483fc3bbd9638fae9
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2016-11-17 21:51:23 +0100

    Adjust trait import script to use fennec id directly

--- bin/import_traits_tsv.py
+++ bin/import_traits_tsv.py
@@ -8,7 +8,7 @@ from optparse import OptionParser
 version = "0.1"
 usage = "Usage: %prog [options] --trait-type=TRAIT_TYPE --user-id=USER_ID FILE.tsv\n"
 usage += "The tsv file has to have the following columns:\n"
-usage += "ncbi_taxid\tvalue\tvalue_ontology\tcitation\torigin_url\tprivate\tcreation_date\tdeletion_date\n"
+usage += "fennec_id\tvalue\tvalue_ontology\tcitation\torigin_url\tprivate\tcreation_date\tdeletion_date\n"
 usage += "only the first two are required"
 parser = OptionParser(version=version, usage=usage)
 parser.add_option("-t", "--trait-type", dest="trait_type", help="type of trait to import", type="string")
@@ -51,20 +51,6 @@ if trait_type_id == -1:
     print("This trait type does not exist in the database. Please create it there")
     sys.exit(1)
 
-def get_ncbi_taxonomy_db_id():
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute("""SELECT db_id FROM db WHERE name='DB:NCBI_taxonomy'""")
-            rows = cur.fetchall()
-            if (len(rows) < 1):
-                return -1
-            return rows[0][0]
-
-ncbi_taxonomy_db_id = get_ncbi_taxonomy_db_id()
-if ncbi_taxonomy_db_id == -1:
-    print("Error no db NCBI Taxonomy - so mapping to organism_ids not possible")
-    sys.exit(1)
-
 def get_or_insert_trait_categorical_value(value, ontology_url):
     with conn:
         with conn.cursor() as cur:
@@ -89,15 +75,6 @@ def get_or_insert_trait_citation(citation):
                 rows = cur.fetchall()
     return rows[0][0]
 
-def get_organism_id(ncbi_taxid):
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT organism_id FROM organism_dbxref od, dbxref WHERE od.dbxref_id = dbxref.dbxref_id AND dbxref.db_id=%s AND dbxref.accession=%s", (ncbi_taxonomy_db_id, ncbi_taxid))
-            rows = cur.fetchall()
-            if (len(rows) < 1):
-                return -1
-    return rows[0][0]
-
 def insert_trait_categorical_entry(row):
     # convert all empty strings to None
     rowNull = list()
@@ -106,25 +83,21 @@ def insert_trait_categorical_entry(row):
             rowNull.append(None)
         else:
             rowNull.append(i)
-    (ncbi_taxid, value, ontology_url, citation, origin_url, private, creation_date, deletion_date) = rowNull
-    organism_id = get_organism_id(ncbi_taxid)
-    if organism_id == -1:
-        print("No organism_id found for NCBI taxid: "+ncbi_taxid)
-    else:
-        trait_categorical_value_id = get_or_insert_trait_categorical_value(value, ontology_url)
-        trait_citation_id = get_or_insert_trait_citation(citation)
-        if private is None:
-            private = options.private
-        with conn:
-            with conn.cursor() as cur:
-                if creation_date is None:
-                    cur.execute("INSERT INTO trait_categorical_entry (trait_type_id, organism_id, trait_categorical_value_id, webuser_id, private, trait_citation_id, origin_url, deletion_date) "+
-                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-                      (trait_type_id, organism_id, trait_categorical_value_id, options.user_id, private, trait_citation_id, origin_url, deletion_date))
-                else:
-                    cur.execute("INSERT INTO trait_categorical_entry (trait_type_id, organism_id, trait_categorical_value_id, webuser_id, private, trait_citation_id, origin_url, creation_date, deletion_date) "+
-                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-                      (trait_type_id, organism_id, trait_categorical_value_id, options.user_id, private, trait_citation_id, origin_url, creation_date, deletion_date))
+    (fennec_id, value, ontology_url, citation, origin_url, private, creation_date, deletion_date) = rowNull
+    trait_categorical_value_id = get_or_insert_trait_categorical_value(value, ontology_url)
+    trait_citation_id = get_or_insert_trait_citation(citation)
+    if private is None:
+        private = options.private
+    with conn:
+        with conn.cursor() as cur:
+            if creation_date is None:
+                cur.execute("INSERT INTO trait_categorical_entry (trait_type_id, fennec_id, trait_categorical_value_id, webuser_id, private, trait_citation_id, origin_url, deletion_date) "+
+                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                  (trait_type_id, fennec_id, trait_categorical_value_id, options.user_id, private, trait_citation_id, origin_url, deletion_date))
+            else:
+                cur.execute("INSERT INTO trait_categorical_entry (trait_type_id, fennec_id, trait_categorical_value_id, webuser_id, private, trait_citation_id, origin_url, creation_date, deletion_date) "+
+                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                  (trait_type_id, fennec_id, trait_categorical_value_id, options.user_id, private, trait_citation_id, origin_url, creation_date, deletion_date))
 
 with open(args[0]) as csvfile:
     reader = csv.reader(csvfile, delimiter="\t")
