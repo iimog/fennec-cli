9356765e1b3c904e8323fde1a8fae017821fb772
Markus Ankenbrand <markus@ankenbrand.me>
2017-01-05 09:20:01 +0100

    Fix path of python script

--- bin/update_iucn_traits.sh
+++ bin/update_iucn_traits.sh
@@ -25,4 +25,4 @@ WEBUSER_ID=$(PGPASSWORD=$DB_PW psql -At -F $'\t' -U $DB_USER -h $DB_HOST -d $DB_
 PGPASSWORD=$DB_PW psql -U $DB_USER -h $DB_HOST -d $DB_NAME -p $DB_PORT -c "UPDATE trait_categorical_entry SET deletion_date=NOW() WHERE trait_type_id=(SELECT id FROM trait_type WHERE type = 'IUCN Threat Status') AND deletion_date IS NULL;"
 
 # import trait table
-python ~/projects/fennec-cli/bin/import_traits_tsv.py -t "IUCN Threat Status" -u $WEBUSER_ID -p --db-user $DB_USER --db-password $DB_PW --db-name $DB_NAME --db-host $DB_HOST --db-port $DB_PORT iucn_traits.tsv
+python `dirname $0`/import_traits_tsv.py -t "IUCN Threat Status" -u $WEBUSER_ID -p --db-user $DB_USER --db-password $DB_PW --db-name $DB_NAME --db-host $DB_HOST --db-port $DB_PORT iucn_traits.tsv
